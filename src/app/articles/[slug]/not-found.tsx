6fc48e2da1bee58f22ad9ccb4c3a01017532dfaa
Chris Maresca <chris@chrismaresca.dev>
2025-01-10 16:56:19 -0500

    Updated framer motion to motion

--- src/app/articles/[slug]/not-found.tsx
+++ src/app/articles/[slug]/not-found.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "motion/react"
 import Link from "next/link"
 
 export default function NotFound() {
